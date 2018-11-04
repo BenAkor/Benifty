969c9c9a71cd8ed80702d58cef7810585a7c081c
BenAkor <ben2lice@gmail.com>
2018-11-04 21:36:50 +0000

    Making the script a function

---
+++ Get-MacAddress.ps1
@@ -0,0 +1,14 @@
+﻿<#
+ .SYNOPSIS
+ Retrieves the Mac Address from a remote or local computer
+ .DESCRIPTION
+ Use WMI object to collect the MacAddress and the manufacturer name of the NIC
+ .PARAMETER -Computername
+ Enter the Computer name or ip address of the machine
+ .EXAMPLE
+ Get-MacAddress BAY-Computer
+#>
+function Get-MacAddress ([string[]] $computername = 'localhost') {
+    Get-WMIObject win32_NetworkAdapter | Select-Object Name, MacAddress, @{l='Max Speed (Mb) '; e={$_.Speed / 1Mb -as [int]}}
+}
+
---
+++ checking dbatools Installed.ps1
@@ -0,0 +1,9 @@
+﻿
+$dbatools = Get-Module -ListAvailable | where {$_.Name -eq 'dbatools'}
+
+if ($dbatools.Name -eq 'dbatools') 
+{ }
+
+elseif ($dbatools.Name -ne 'dbatools') 
+
+{Install-Module dbatools}
